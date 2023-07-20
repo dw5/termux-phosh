702a3949d665c655042c880cc672bd6b2d77a9b3
dw5 <29355584+dw5@users.noreply.github.com>
2023-07-20 20:21:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 # idea concept: WIP get apk or reuse https://github.com/Ilya114/Box64Droid https://github.com/termux/termux-x11/ https://github.com/termux/termux-x11/actions/workflows/debug_build.yml
 # https://youtu.be/1UUGgEVuA3k https://youtu.be/lB9eqixqSS8
 pkg install proot-distro x11-repo xwayland 
+proot-distro install alpine
+
+proot-distro install alpine
+apk update && apk add phosh
+
+exit #on termux
+termux-x11 :1
+proot-distro login alpine --shared-tmp
+export DISPLAY=:1
