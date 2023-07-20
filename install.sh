5147a46b8797db4ce410bed6f2c376fa9ba44f5d
dw5 <29355584+dw5@users.noreply.github.com>
2023-07-20 19:59:43 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+# idea concept: WIP get apk or reuse https://github.com/Ilya114/Box64Droid https://github.com/termux/termux-x11/ https://github.com/termux/termux-x11/actions/workflows/debug_build.yml
+# https://youtu.be/1UUGgEVuA3k https://youtu.be/lB9eqixqSS8
+pkg install proot-distro x11-repo xwayland 
